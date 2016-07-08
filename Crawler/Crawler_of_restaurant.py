a010f50f2d2c614c960fe92cfc42e4be10522fa8
david <davidtnfsh@gmail.com>
2016-07-08 16:52:52 +0800

    修復gomaji爬蟲，改用select + arduino與raspeberrypi的code

--- Crawler/Crawler_of_restaurant.py
+++ Crawler/Crawler_of_restaurant.py
@@ -1,60 +1,34 @@
 #!/usr/bin/env python3
-import requests, json
+import requests, json, pyprind
 from bs4 import BeautifulSoup
 
-def startCrawler():
+def startCrawler(fileName):
     prod_dict={}
     base_url = "http://www.gomaji.com/"
-    fileName = 'gomaji.json'
     json_arr = []
 
     res = requests.get('http://www.gomaji.com/index.php?city=Taichung&tag_id=99')
-    #print(res.text)
     soup = BeautifulSoup(res.text)
-    liTag = soup.find_all("li","box-shadow2px ")
-    for i in liTag:
+    # liTag = soup.find_all("li","box-shadow2px ")
+
+    recipNum = len(soup.select('ul.deal16 li.box-shadow2px'))
+    ProgreBar = pyprind.ProgBar(recipNum,title = "共 %d 個餐廳要處理" % recipNum)
+    #建立一個進度條物件
+
+    for i in soup.select('ul.deal16 li.box-shadow2px'):
         href = i.find('a')['href']# 把a的href屬性的值抓出來
         href = base_url+href
 
-        name = i.find('a').find('div','program').find('h1')# find可以找到他的child那一層
+        name = i.find('a').find('div','boxc').find('h2')# find可以找到他的child那一層
         name = name.text.strip()
 
         d = {name: href}
         json_arr.append(d)
-    
-    start_json(fileName)
-    to_json(fileName, json_arr)
-    end_json(fileName)        
+        ProgreBar.update()#item_id可以讓使用者追蹤到底執行到第幾個ID
+            #ID通常是放for loop裏面的變數，update()會讓進度條更新
 
-
-def start_json(json_path):
-    with open(json_path, 'w' ,encoding = 'UTF-8') as json_file:
-        json_file.truncate()#如果沒有傳入參數的話，就會本全文清空
-        #若傳入整數n的話，是指把n位置以後的文字都刪掉
-        json_file.write('[')#單純只是寫入而已
-
-def to_json(json_path,arr,notFirst = False):
-    # print(arr)
-    with open(json_path, 'a', encoding='UTF-8') as json_file:
-        #with 述句執行完畢後會自動關檔，後面的as 則是把開檔完的reference指派給as 後的變數
-        #as裡面的名稱在外部是看不到的，是區域變數
-        for d in arr:
-            json_str = json.dumps(d, ensure_ascii=False, sort_keys=True)
-            #在這裡使用到json的module,dump是轉存，將python的物件型態轉成json的物件型態
-            #因為json是js的型態；ensure_ascii若為true(預設)
-            #就會確保所有輸入的字元都是ascii，若非則跳過那個字元
-            #設為false就會照原樣輸出
-            #sort_keys預設為false，功用為把key做排序
-            json_file.write('{}{}'.format((',' if notFirst else ''), json_str))
-            #str.format()這個函式，會在{}裡面填入字串，{}裡面可以放index或key名稱
-            notFirst = True
-
-def end_json(json_path):
-    with open(json_path, 'a' ,encoding = 'UTF-8') as json_file:
-        json_file.write(']')
+    with open(fileName, 'w', encoding='UTF-8') as f:
+        json.dump(json_arr, f)
 
 if __name__  ==  "__main__":
-    startCrawler()
-    #print(course_unit)
-    # to_json('ptt_comment.json',course_unit,False)
-    # end_json('ptt_comment.json')
+    startCrawler('gomaji.json')
