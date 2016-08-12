0696cef5c9142e7e3a30f66cded245096c0e6428
Kobetin <at850528@gmail.com>
2016-08-12 17:10:58 +0800

    add django

---
+++ django/first/time2eat/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, url
+import time2eat 
+urlpatterns = patterns('time2eat.views',
+  url(r'^$','time2eat'),#這樣做似乎是對應到首頁
+  url(r'^all_list/$', 'all_list'),
+  url(r'^all_picture/$', 'all_picture'),
+  url(r'^inside_resturant/$', 'inside_resturant'),
+  url(r'^inside_resturant/$', 'inside_resturant'),
+  url(r'^nearby_picture/$', 'nearby_picture'),
+  url(r'^nearby_list/$', 'nearby_list'),
+  url(r'^nearby_listpic/$', 'nearby_listpic'),
+)
