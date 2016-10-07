5a14b80a33df72b963800e3019485dbfddb83e24
IJuLeeeeee <leoviclee85@gmail.com>
2016-10-07 22:53:30 +0800

    Add files via upload

--- android/src/main/java/faith/stufinite/time2eat/time2eat/MainActivity.java
+++ android/src/main/java/faith/stufinite/time2eat/time2eat/MainActivity.java
@@ -1,8 +1,12 @@
 package faith.stufinite.time2eat.time2eat;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnmenu=(Button)findViewById(R.id.btnmenu);
+        btnmenu.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent it = new Intent();
+                it.setClass(MainActivity.this,Menu.class);
+                //2跳到3
+                startActivity(it);
+            }
+        });
+        EditText editText = findViewById(R.id.et);
+        Button btnquest=(Button)findViewById(R.id.btnquest);
+        btnquest.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "這是查詢", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
---
+++ android/src/main/java/faith/stufinite/time2eat/time2eat/Menu.java
@@ -0,0 +1,13 @@
+package faith.stufinite.time2eat.time2eat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Menu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
