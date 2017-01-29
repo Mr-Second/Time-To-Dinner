f658540bab2f6728e6e3473ddac51ff5b9bd0940
davidtnfsh <davidtnfsh@gmail.com>
2017-01-29 17:38:36 +0800

    finish join_order_list api

--- t2e/views.py
+++ t2e/views.py
@@ -9,6 +9,7 @@ from t2e.view.restaurant_prof import restaurant_prof
 from t2e.view.restaurant_menu import restaurant_menu
 from t2e.view.purchase import purchase
 from t2e.view.join_order import join_order
+from t2e.view.join_order_list import join_order_list
 import requests, json
 
 def index(request):
