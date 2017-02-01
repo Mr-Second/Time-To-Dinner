1d1053f0e0f36859afc6c4e778db8fdd78a105da
davidtnfsh <davidtnfsh@gmail.com>
2017-02-01 12:39:07 +0800

    rename project into inferno

--- inferno/wsgi.py
+++ inferno/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for time2eat project.
+WSGI config for inferno project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "time2eat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inferno.settings")
 
 application = get_wsgi_application()
