366863a0c69a17403fcf06c63dad84b77749bd14
davidtnfsh <davidtnfsh@gmail.com>
2017-03-18 00:35:50 +0800

    首頁改成三種情況，餐廳列表、所有揪團訂單、特定餐廳訂單

--- time2eatWeb/views.py
+++ time2eatWeb/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render_to_response, render
 from gluttonyTw.models import ResProf
+from gluttonyTw.models import Order
 from djangoApiDec.djangoApiDec import queryString_required
 
 def index(request):
-	res = ResProf.objects.all()
-	print(res)
-	print(res[0].avatar.url)
-	return render_to_response('time2eatWeb/index.html', locals())
+    if 'chkgroup' in request.GET and request.GET['chkgroup']=='T':
+        if 'res_id' in request.GET and request.GET['res_id']:
+            order =Order.objects.filter(restaurant__id=request.GET['res_id'])
+        else:
+            order =Order.objects.all()
+    else:
+        res = ResProf.objects.all()
+
+    jsval = 11111 # just demo to sonson
+    return render_to_response('time2eatWeb/index.html', locals())
 
 def inside_resturant(request):
-	if 'res_id' in request.GET and request.GET['res_id']!='':
-		res = ResProf.objects.get(id=request.GET['res_id'])
-	return render_to_response('time2eatWeb/inside_resturant.html',locals())
+    if 'res_id' in request.GET and request.GET['res_id']!='':
+        res = ResProf.objects.get(id=request.GET['res_id'])
+    return render_to_response('time2eatWeb/inside_resturant.html',locals())
 
 def check(request):
-	return render_to_response('time2eatWeb/check.html',locals())
+    return render_to_response('time2eatWeb/check.html',locals())
 
 @queryString_required(['res_id'])
 def purchase(request):
-	from gluttonyTw.view.get_user import get_user
-	res = ResProf.objects.get(id=request.GET['res_id'])
-	EatU, upperuser = get_user(request)
-	return render(request, 'time2eatWeb/purchase.html', locals())
+    from gluttonyTw.view.get_user import get_user
+    res = ResProf.objects.get(id=request.GET['res_id'])
+    EatU, upperuser = get_user(request)
+    return render(request, 'time2eatWeb/purchase.html', locals())
