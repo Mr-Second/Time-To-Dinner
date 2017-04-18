40878d5de0edd456c752a34cfddc7061c047835a
davidtnfsh <davidtnfsh@gmail.com>
2017-04-18 23:17:17 +0800

    優化共用板模templates/infernoWeb裡面的html + 重新命名time2eat -> gluttonyWeb

--- infernoWeb/urls.py
+++ infernoWeb/urls.py
@@ -1,14 +1,14 @@
 
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
-from infernoWeb import views
+from infernoWeb.view import gluttony
 from infernoWeb.view import sloth
 urlpatterns = [
-  url(r'^$',views.index, name='index'),#這樣做似乎是對應到,首頁
-  url(r'^inside_resturant/$', views.inside_resturant, name='inside_resturant'),
-  url(r'^purchase/$', views.purchase, name='purchase'),
-  url(r'^check/$', views.check, name='check'),
-  url(r'^boss/$', views.boss, name='boss'),
+  url(r'^gluttony$',gluttony.index, name='gluttony'),#這樣做似乎是對應到,首頁
+  url(r'^inside_resturant/$', gluttony.inside_resturant, name='inside_resturant'),
+  url(r'^purchase/$', gluttony.purchase, name='purchase'),
+  url(r'^check/$', gluttony.check, name='check'),
+  url(r'^boss/$', gluttony.boss, name='boss'),
 ] + [
   # 課程心得的部份
   url(r'^sloth$',sloth.index, name='sloth')
