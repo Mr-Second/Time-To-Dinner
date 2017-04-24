0cb8988fb57ab9265e721e76244935cce7cb3ba4
david <davidtnfsh@gmail.com>
2017-04-24 22:29:38 +0800

    完成留言form post的功能 + 頁面都會顯示這是哪間求職網站的資訊

--- infernoWeb/view/arrogant.py
+++ infernoWeb/view/arrogant.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, render
 from djangoApiDec.djangoApiDec import queryString_required
 from arrogant.models import Job, Comment
+import datetime
 
 def index(request):
     jlist = Job.objects.all()
@@ -10,5 +11,6 @@ def index(request):
 def inside(request):
     id = request.GET['id']
     j = Job.objects.get(id=id)
-    # comments = Comment.objects.filter(c)
-    return render_to_response('arrogantWeb/inside.html', locals())
+    if request.method == 'POST' and request.POST:
+    	Comment.objects.create(Job=j, create=datetime.datetime.now(), raw=request.POST['comments'])
+    return render(request, 'arrogantWeb/inside.html', locals())
