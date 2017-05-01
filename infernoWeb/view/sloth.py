d0cdce7e093fb59b34908adc7b1212b20ca52fea
david <davidtnfsh@gmail.com>
2017-05-01 11:23:11 +0800

    心得表單功能完成

--- infernoWeb/view/sloth.py
+++ infernoWeb/view/sloth.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, render
 from djangoApiDec.djangoApiDec import queryString_required
 from slothTw.models import Course, Comment
 from django.urls import reverse
-import datetime
+import datetime, json
 
 def index(request):
     clist = Course.objects.all()
@@ -13,7 +13,18 @@ def inside(request):
     id = request.GET['id']
     c = Course.objects.get(id=id)
     if request.method == 'POST' and request.POST:
-    	Comment.objects.create(course=c, create=datetime.datetime.now(), raw=request.POST['comments'])
+        if 'rating' in request.POST:
+            data = json.loads(request.POST.dict()['rating'])
+            amount = c.feedback_amount + 1
+            modelDict = {}
+            modelDict['feedback_freedom'] = (c.feedback_freedom*(amount-1) + (data[0]*3/4 + data[1]/4)) /amount
+            modelDict['feedback_GPA'] = (c.feedback_GPA*(amount-1) + data[2]) / amount
+            modelDict['feedback_easy'] = (c.feedback_easy*(amount-1) + (data[3]/12 + data[4]/12  + data[7]*9/12 + data[8]/12)) / amount
+            modelDict['feedback_knowledgeable'] = (c.feedback_knowledgeable*(amount-1) + data[6]) / amount
+            modelDict['feedback_FU'] = (c.feedback_FU*(amount-1) + data[5]) / amount
+            Course.objects.update_or_create(id=id, defaults=modelDict)
+        else:
+            Comment.objects.create(course=c, create=datetime.datetime.now(), raw=request.POST['comments'])
     comments = Comment.objects.filter(course=c)
     urlPattern = reverse('infernoWeb:slothquestion') + '?id={}'.format(id)
     return render(request, 'slothWeb/inside.html', locals())
