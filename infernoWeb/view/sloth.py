bee5179d00f09d8693f167438aa78976559ebb20
david <davidtnfsh@gmail.com>
2017-06-12 22:35:07 +0800

    give warning toastr if user re-submit comment to course + recode user log of visiting pages when navigating into inside.html

--- infernoWeb/view/sloth.py
+++ infernoWeb/view/sloth.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render_to_response, render, redirect
-from django.http import JsonResponse, Http404
+from django.http import JsonResponse, Http404, HttpResponse
 from djangoApiDec.djangoApiDec import queryString_required
 from slothTw.models import Course, Comment
-from slothTw.views import CreateComment
+from slothTw.views import CreateComment, logPage
 from infernoWeb.models import User
 import datetime, json
 
@@ -17,9 +17,11 @@ def index(request):
 def inside(request):
     id = request.GET['id']
     c = Course.objects.get(id=id)
-    if request.method == 'POST' and request.POST['comments']:
-        CreateComment(request)
-        return redirect(request.get_full_path())
+    if request.method == 'POST': 
+        logPage(request)
+        if 'comments' in request.POST:
+            if CreateComment(request)==False:
+                return HttpResponse("SORRY 目前只開放留言一次喔~~ 未來會再依照情況調整")
     school = c.school
     urlpattern = '/infernoWeb/sloth/search'
     return render(request, 'slothWeb/inside.html', locals())
